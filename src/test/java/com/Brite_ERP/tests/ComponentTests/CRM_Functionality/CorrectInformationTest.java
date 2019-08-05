c615538c66e1f9fa3730fb8ba48fee96868d9e4c
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-08-05 17:32:41 -0400

    correct information html report update

--- src/test/java/com/Brite_ERP/tests/ComponentTests/CRM_Functionality/CorrectInformationTest.java
+++ src/test/java/com/Brite_ERP/tests/ComponentTests/CRM_Functionality/CorrectInformationTest.java
@@ -15,25 +15,43 @@ public class CorrectInformationTest extends TestBase {
     @Test
     public void ExpectedValueTest(){
 
+        extentLogger =report.createTest("Expected Revenue Value Test ");
 
+        extentLogger.info("Navigating to Signin page");
         pages.login().SignIn.click();
+
+        extentLogger.info("Logging in with valid user credentials");
         pages.login().loginCRM(ConfigurationReader.getProperty("CRMusername"),ConfigurationReader.getProperty("CRMpassword"));
         BrowserUtils.wait(3);
+
+        extentLogger.info("Navigating to CRM module");
         pages.homePage().CRM.click();
         BrowserUtils.wait(2);
+
+        extentLogger.info("Clicking Pivot board");
         List<WebElement> grid=pages.homePage().Grids;
         WebElement Pivot=grid.get(17);
         Pivot.click();
         BrowserUtils.wait(3);
+
+        extentLogger.info("Clicking Total option");
         pages.homePage().totalNew.click();
+
+        extentLogger.info("Selecting Opportunity option");
         pages.homePage().Opportunity.click();
         BrowserUtils.wait(1);
+
+        extentLogger.info("Clicking List board");
         String expectedRevenueInPivot=pages.homePage().expectedRevenue2Pivot.getText();
         WebElement List=grid.get(11);
         List.click();
         BrowserUtils.wait(3);
+
+        extentLogger.info("Verifying expected revenue value");
         String expectedRevenueInList=pages.homePage().getExpectedRevenue2List.getText();
         Assert.assertEquals(expectedRevenueInPivot,expectedRevenueInList);
+
+        extentLogger.pass("Expected revenue value test is passed");
 
 
 
@@ -46,21 +64,31 @@ public class CorrectInformationTest extends TestBase {
     @Test
     public void expectedRevenueSumTest(){
 
+        extentLogger=report.createTest("Expected Revenue Value Sum Test");
 
+        extentLogger.info("Nevigating to Signin page");
         pages.login().SignIn.click();
+
+        extentLogger.info("Loggin in with valid user credentials");
         pages.login().loginCRM(ConfigurationReader.getProperty("CRMusername"),ConfigurationReader.getProperty("CRMpassword"));
         BrowserUtils.wait(3);
+
+        extentLogger.info("Navigating to CRM module");
         pages.homePage().CRM.click();
         BrowserUtils.wait(2);
+
+        extentLogger.info("Clicking Pivot board");
         List<WebElement> grid=pages.homePage().Grids;
         WebElement Pivot=grid.get(17);
         Pivot.click();
         BrowserUtils.wait(3);
+
+        extentLogger.info("Clicking Total option");
         pages.homePage().totalNew.click();
+
+        extentLogger.info("Selecting Opportunity option");
         pages.homePage().Opportunity.click();
         BrowserUtils.wait(1);
-
-
 
         String totalER=pages.homePage().totalRevenue.getText();
         String newTotalER=totalER.substring(0,2)+totalER.substring(3);
@@ -77,8 +105,10 @@ public class CorrectInformationTest extends TestBase {
 
         Double sum=erOne+erTwo+erThree;
 
-
+        extentLogger.info("Verifying expected revenue value sum");
         Assert.assertEquals(totalExpectedRevenue,sum);
+
+        extentLogger.pass("Expected revenue value sum test is passed");
 
 
 
